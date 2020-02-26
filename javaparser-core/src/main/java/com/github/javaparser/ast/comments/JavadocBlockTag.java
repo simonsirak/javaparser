fa9a4c6c28d5f06d610a32c14946a62e563aa494
David Johansson <davidjo2@kth.se>
2020-02-26 08:49:20 +0100

    feat #1
    Implemented preliminary JavadocBlockTag

--- javaparser-core/src/main/java/com/github/javaparser/ast/comments/JavadocBlockTag.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/comments/JavadocBlockTag.java
@@ -4,14 +4,69 @@ import com.github.javaparser.TokenRange;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.visitor.GenericVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.observer.ObservableProperty;
 
 /**
- * Created by simonsirak on 2020-02-25.
+ * A JavaDoc block tag.
+ * 
+ * @author Simon Sirak & David Johansson
  */
-public class JavadocBlockTag extends Node {
+public class JavadocBlockTag extends Node { 
 
-    protected JavadocBlockTag(TokenRange tokenRange) {
+    private JavadocDescription description;
+    private BlockTagType type;
+
+    public enum BlockTagType {
+        AUTHOR,
+        DEPRECATED,
+        EXCEPTION,
+        PARAM,
+        RETURN,
+        SEE,
+        SERIAL,
+        SERIAL_DATA,
+        SERIAL_FIELD,
+        SINCE,
+        THROWS,
+        VERSION,
+        UNKNOWN;
+    }
+
+    protected JavadocBlockTag(TokenRange tokenRange, JavadocDescription description, BlockTagType type) {
         super(tokenRange);
+        setDescription(description);
+        setType(type);
+    }
+
+    /**
+     * Set the description of the block tag     
+     * @param description describes the tag
+     * @return new version of current object
+     */
+    public JavadocBlockTag setDescription(JavadocDescription description) {         
+        if (description == this.description) {            
+            return this;
+        }
+        notifyPropertyChange(ObservableProperty.ANNOTATIONS, this.description, description);
+        if (this.description != null)
+            this.description.setParentNode(null);
+        this.description = description;
+        setAsParentNodeOf(description);
+        return this;
+    }
+
+    /**
+     * Set the type of the block tag     
+     * @param type the block tag type
+     * @return new version of current object
+     */
+    public JavadocBlockTag setType(BlockTagType type) {         
+        if(type == null) {
+            this.type = BlockTagType.UNKNOWN;
+        }        
+        notifyPropertyChange(ObservableProperty.ANNOTATIONS, this.type, type);
+        this.type = type;        
+        return this;
     }
 
     @Override
--- javaparser-core/src/main/java/com/github/javaparser/ast/comments/JavadocDescription.java
+++ javaparser-core/src/main/java/com/github/javaparser/ast/comments/JavadocDescription.java
@@ -4,18 +4,32 @@ import com.github.javaparser.TokenRange;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.visitor.GenericVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.observer.ObservableProperty;
+import static com.github.javaparser.utils.Utils.assertNotNull;
 
 /**
  * Created by simonsirak on 2020-02-25.
  */
 public class JavadocDescription extends Node {
+    private String description;
 
-    public JavadocDescription(){
-        super(null);
+    public JavadocDescription() {
+        this(null, "");
     }
 
-    protected JavadocDescription(TokenRange tokenRange) {
+    public JavadocDescription(TokenRange tokenRange, String content){
         super(tokenRange);
+        setDescription(content);
+    }
+
+    public JavadocDescription setDescription(final String description) {
+        assertNotNull(description);
+        if (description == this.description) {
+            return (JavadocDescription) this;
+        }
+        notifyPropertyChange(ObservableProperty.CONTENT, this.description, description);
+        this.description = description;
+        return this;
     }
 
     @Override
